01c34136905db4203f69e4eeaa3c94a9723e8ee5
ovv77 <ovv77@yandex.ru>
2020-06-10 19:28:05 +0400

    решил задачи до 4_5

--- 04_data_structures/task_4_2.py
+++ 04_data_structures/task_4_2.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 4.2
 
 Преобразовать строку mac из формата XXXX:XXXX:XXXX в формат XXXX.XXXX.XXXX
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
+"""
 
-mac = 'AAAA:BBBB:CCCC'
+mac = "AAAA:BBBB:CCCC"
+print(mac.replace(':', '.', 100))
--- 04_data_structures/task_4_6.py
+++ 04_data_structures/task_4_6.py
@@ -1,17 +1,30 @@
 # -*- coding: utf-8 -*-
-'''
+"""
 Задание 4.6
 
 Обработать строку ospf_route и вывести информацию на стандартный поток вывода в виде:
-Protocol:              OSPF
-Prefix:                10.0.24.0/24
-AD/Metric:             110/41
-Next-Hop:              10.0.13.3
-Last update:           3d18h
-Outbound Interface:    FastEthernet0/0
+Prefix                10.0.24.0/24
+AD/Metric             110/41
+Next-Hop              10.0.13.3
+Last update           3d18h
+Outbound Interface    FastEthernet0/0
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
-'''
+"""
 
-ospf_route = 'O        10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0'
+ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+sub_lst = ' '.join(' '.join(ospf_route.split('via')).split(',')).split(']')
+lst = ' '.join(' '.join(sub_lst).split('[')).split()
+
+
+print(lst)
+
+
+
+tmp = '''
+Prefix:\t\t\t {}
+AD/Metric\t\t\t {}
+'''
+print(tmp.format('111', '222'))
+
